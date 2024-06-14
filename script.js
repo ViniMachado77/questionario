cafed2564f80c81bc7b9c24913e591bce61891f6
ViniMachado77 <107115421+ViniMachado77@users.noreply.github.com>
2024-06-14 15:34:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "PARABENS!!! ate que seu gosto não é de todo ruim...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
